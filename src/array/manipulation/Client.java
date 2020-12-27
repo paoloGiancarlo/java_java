aa6f302f38f3dd4dacb2616b00bdccbf87e8fe71
Paolo Ignacio <igna0029@algonquinlive.com>
2020-12-27 05:42:21 -0500

    Client class basically show how to work with arrays, by adding elements to a Array Client.

---
+++ src/array/manipulation/Client.java
@@ -0,0 +1,54 @@
+package array.manipulation;
+
+import java.util.Scanner;
+
+
+public class Client {
+    private String name;
+    private int clientNumber;
+
+    public Client() {
+        this("unassigned", -1);
+    }
+
+    public Client(String name, int clientNumber) {
+        this.name = name;
+        this.clientNumber = clientNumber;
+
+    }
+
+    public static void main(String[] args) {
+
+        final int numberOfClients = 2;
+        Client[] clients = new Client[numberOfClients];
+
+        for (int i = 0; i < numberOfClients; i++) {
+
+            clients[i] = new Client();
+            System.out.println("Enter information for Client " + (i + 1));
+            clients[i].keyboardClientInfo();
+        }
+
+        System.out.println("List of Client \n ");
+        for (int i = 0; i < numberOfClients; i++) {
+
+//            clients[i] = new Client();
+            clients[i].displayClientInfo();
+        }
+
+    }
+
+    public void keyboardClientInfo() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Client name: ");
+        name = input.nextLine();
+        System.out.println("Client number: ");
+        clientNumber = input.nextInt();
+
+    }
+
+    public void displayClientInfo() {
+        System.out.println("Client Name: " + name + " Client Number" + clientNumber);
+    }
+}
+
